ffa2ec53dfefee43104184a326fc54fe5716520f
清川　裕太郎 <yuu7353@gmail.com>
2020-08-27 23:07:27 +0900

    JSでクイズ（BootStrap)

---
+++ html/app.js
@@ -0,0 +1,75 @@
+const quiz = [
+  {
+    question: '機械学習に使われる言語はどれ？',
+    answers: [ 'Ruby', 'PHP', 'JavaScript', 'python'],
+    correct: 'python'
+  }, {
+    question: 'JavaScriptの上位互換と言われている言語はどれ？',
+    answers: [ 'Vue.js', 'React.js', ' TypeScript', 'Java'],
+    correct: ' TypeScript'
+  }, {
+    question: '一番使われているエディタはどれ？',
+    answers: [ 'Atom', 'vscode', 'Brackets', 'codepen'],
+    correct: 'vscode'
+  }
+];
+
+const $window = window;
+const $doc = document;
+const $question = $doc.getElementById('js-question');
+const $buttons = $doc.querySelectorAll('.btn');
+
+const quizLen = quiz.length;
+let quizCount = 0;
+let score = 0;
+
+const init = () => {
+  $question.textContent = quiz[quizCount].question;
+  
+  const buttonLen = $buttons.length;
+  let btnIndex = 0;
+  
+  while(btnIndex < buttonLen){
+    $buttons[btnIndex].textContent = quiz[quizCount].answers[btnIndex];
+    btnIndex++;
+  }
+};
+
+const goToNext = () => {
+  quizCount++;
+  if(quizCount < quizLen){
+    init(quizCount);
+  } else {
+    // $window.alert('クイズ終了！');
+    showEnd();
+  }
+};
+
+const judge = (elm) => {
+  if(elm.textContent === quiz[quizCount].correct){
+    $window.alert('正解!');
+    score++;
+  } else {
+    $window.alert('不正解!');
+  }
+  goToNext();
+};
+
+const showEnd = () => {
+  $question.textContent = '終了！あなたのスコアは' + score + '/' + quizLen + 'です';
+  
+  const $items = $doc.getElementById('js-items');
+  $items.style.visibility = 'hidden';
+};
+
+init();
+
+let answersIndex = 0;
+let answersLen = quiz[quizCount].answers.length;
+
+while(answersIndex < answersLen){
+  $buttons[answersIndex].addEventListener('click', (e) => {
+    judge(e.target);
+  });
+  answersIndex++;
+}
